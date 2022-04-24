efaae0c0b0093a34bbaec356009501f191f92c50
hh15248 <82835053+hh15248@users.noreply.github.com>
2022-04-24 16:53:54 -0500

    Delete restuarant directory

--- restuarant/waitlist/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Wait
-
-class WaitAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'completed')
-
-# Register your models here.
-
-admin.site.register(Wait, WaitAdmin)
--- restuarant/waitlist/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Wait
-
-class WaitSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Wait
-        fields = ('id', 'title', 'description', 'completed')
